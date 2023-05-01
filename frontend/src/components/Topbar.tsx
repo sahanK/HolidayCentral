c094fa0fd095c90c9f367e88d37c68cefb28107c
Sahan Kaushalya <sahanwalpita@liveroom.xyz>
2023-05-01 23:11:26 +0530

    Added flights pages for staff

---
+++ frontend/src/components/Topbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Topbar: React.FC = () => {
+  return (
+    <div className='w-full h-14 flex flex-row justify-end item-center border-b-2 border-gray-100'>
+      <h3 className='text-grayscale-100 border-2 border-white'>Sahan Kaushalya</h3>
+    </div>
+  );
+};
+
+export default Topbar;
