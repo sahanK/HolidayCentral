1f94a4f1e0593af1e5db8bbc8b8c0d69821492b5
Sahan Kaushalya <sahankaushalya01@gmail.com>
2023-05-01 14:49:16 +0530

    Added data files

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use('/api/v1/auth', auth);
 // Error handling middleware
 app.use(errorHandler);
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running at port ${8080}`);
